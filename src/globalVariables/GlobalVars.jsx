4cdb2018540b8b95e420447e557bfa1baa388e0f
unknown <nosrepsiht2002@gmail.com>
2024-03-02 20:43:46 +0500

    update commit

--- src/globalVariables/GlobalVars.jsx
+++ src/globalVariables/GlobalVars.jsx
@@ -1,6 +1,6 @@
 module.exports = {
-    backend_server: 'http://localhost:8800',
+    // backend_server: 'http://localhost:8800',
     // backend_server: 'https://af2af6cd435fec.lhr.life',
-    // backend_server: 'https://sushilifedemo.onrender.com',
+    backend_server: 'https://sushilifedemo.onrender.com',
     test_str: "testing..."
 };
