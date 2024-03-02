e0ca96e69acfdd54c5e98839615a30b72ffd3e4a
unknown <nosrepsiht2002@gmail.com>
2024-03-03 01:14:23 +0500

    update commit

--- src/globalVariables/GlobalVars.jsx
+++ src/globalVariables/GlobalVars.jsx
@@ -1,6 +1,7 @@
 module.exports = {
     // backend_server: 'http://localhost:8800',
     // backend_server: 'https://af2af6cd435fec.lhr.life',
-    backend_server: 'https://sushilifedemo.onrender.com',
+    // backend_server: 'https://sushilifedemo.onrender.com',
+    backend_server: 'https://sushilifebackendtest.netlify.app',
     test_str: "testing..."
 };
